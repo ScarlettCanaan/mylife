e7fd7d213f40ac3c11d704bd496a00d739bd82b4
ScarlettCanaan <drynessgarden495@gmail.com>
2016-06-13 23:40:17 -0700

    Upgrade main

--- data_structure/hwFinal/main.c
+++ data_structure/hwFinal/main.c
@@ -1,20 +1,49 @@
 #include "contacts.h"
-#include <malloc.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main()
+#define MAX_BUFFER 	100
+
+int main(int argc, char const *argv[])
 {
-	contPtr info = (contPtr)malloc(sizeof(struct contacts));
-	info->name = "1";
-	vertexPtr rear = initVertex(info);
-	rear = addVertexRelationOnName(rear, "1", "4");
-	rear = addVertexRelationOnName(rear, "4", "2");
-	rear = addVertexRelationOnName(rear, "2", "1");
-	rear = addVertexRelationOnName(rear, "5", "1");
-	rear = addVertexRelationOnName(rear, "2", "3");
-	vertexPtr target = getVertexBasedOnName(rear, "4");
-	vertexPtr src = getVertexBasedOnName(rear, "2");
-	bool e = DFS(src, target);
-	e = 0;
-	e = BFS(src, target);
+	FILE *file;
+	vertexPtr rear = NULL;
+	char target1[100], target2[100];
+	char* buf = (char*)malloc(sizeof(char) * MAX_BUFFER);
+	for (int i = 0; i < argc; ++i)
+	{
+		char* arg = argv[i];
+		if ((file = fopen(arg, "r")) == NULL)
+		{
+			printf("Cannot open file %s!\n", argv[0]);
+		}
+		else
+		{
+			contPtr info = (contPtr)malloc(sizeof(struct contacts));
+			info->name = arg;
+			if (rear == NULL)
+			{
+				rear = initVertex(info);
+			}
+			while(fgets(buf, MAX_BUFFER, file) != NULL)
+			{
+				rear = addVertexRelationOnName(rear, arg, buf);
+			}			
+		}
+
+	}
+	while(1)
+	{
+		printf("Please input two target name:\n");
+		scanf("%s%s", target1, target2);
+		vertexPtr target = getVertexBasedOnName(rear, target1);
+		vertexPtr src = getVertexBasedOnName(rear, target2);
+		bool e = DFS(src, target);
+		if (e == FALSE)
+		{
+			printf("They don't have any relationship on the graph\n");
+		}
+	}
+	/* code */
 	return 0;
 }
